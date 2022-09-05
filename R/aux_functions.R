c3cab1f983b444eab9963da44cccbc27353b4658
Lewis Spurgin <lewisspurgin@gmail.com>
2022-09-05 18:13:04 +0100

    world bank indicator initial function

--- R/aux_functions.R
+++ R/aux_functions.R
@@ -38,3 +38,60 @@ euclidean <- function(a, b){
   out <- sqrt(sum((a - b)^2))
   return(out)
 }
+
+
+#' Get world bank indicators
+#'
+#' @description Get world bank indicators, and transform values in USD per capita and in %GDP to totals in USD
+#'
+#' @param world_bank_raw Raw world bank development indicator data
+#' @param year The year to get the data from
+#' @param indicator_set a vector of indicator codes to filter
+#' @return A dataframe of cleaned indicators
+#' @importFrom dplyr select filter mutate pull
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @examples
+#'
+#' initial_indicator_set <- c(
+#' 'GC.TAX.TOTL.GD.ZS',
+#' 'MS.MIL.XPND.CD',
+#' 'NY.ADJ.AEDU.CD',
+#' 'SH.XPD.CHEX.PC.CD',
+#' 'ST.INT.XPND.CD',
+#' 'NE.CON.GOVT.CD'
+#' )
+#'
+#' get_wb_indicators(world_bank_raw,2017,initial_indicator_set)
+#'
+#'
+
+get_wb_indicators <- function(world_bank_raw,year,initial_indicator_set)
+{
+  world_bank_raw[,"value"] <- world_bank_raw[,as.character(year)]
+
+  selected_indicators <- world_bank_raw %>%
+    select(code = `Indicator Code`,
+           name = `Indicator Name`,
+           value) %>%
+    filter(code %in% initial_indicator_set)
+
+  pop <- world_bank_raw %>%
+    filter(`Indicator Code` == 'SP.POP.TOTL') %>%
+    pull(value)
+
+  gdp <- world_bank_raw %>%
+    filter(`Indicator Code` == 'NY.GDP.MKTP.CD') %>%
+    pull(value)
+
+  cleaned_indicators <- selected_indicators %>%
+    mutate(value = case_when(str_detect(code,'.ZS') ~ (value/100)*gdp,
+                             TRUE ~ value)) %>%
+    mutate(value = case_when(str_detect(code,'.PC.') ~ (value)*pop,
+                             TRUE ~ value))
+
+  return(cleaned_indicators)
+
+}
+
