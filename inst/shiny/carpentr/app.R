8a767b21951fc8a9e59586e370090d0eebb1986c
Lewis Spurgin <lewisspurgin@gmail.com>
2022-09-07 21:48:23 +0100

    tidy plots

--- inst/shiny/carpentr/app.R
+++ inst/shiny/carpentr/app.R
@@ -3,11 +3,12 @@ library(shinyWidgets)
 library(bslib)
 library(carpentr)
 library(tidyverse)
+library(cowplot)
 
 indicator_survey_matrix<- read.csv('data-raw/indicators_survey_matrix.csv',row.names = 1,check.names = FALSE)
 indicator_survey_matrix <- as.matrix(indicator_survey_matrix)
 indicator_values <- get_wb_indicators(2000:2017,data_dir = 'data-raw') %>%
-    mutate(value = value/1000000)
+  mutate(value = value/1000000)
 indicator_raw <- read_csv('data-raw/indicators.csv')
 
 qs <- colnames(indicator_survey_matrix)
@@ -16,164 +17,183 @@ inds <- rownames(indicator_survey_matrix)
 ui <- fluidPage(title = "Carbon pricing and the energy transition",
                 tags$style(type = "text/css", "a{color: #044278;}"),
                 setSliderColor(rep('#696969',3),c(1,2,3)),
-    #theme = "bootstrap.css",
-    titlePanel(h1("Carbon pricing and the energy transition",style = "color:#cd2973")),
-    mainPanel(
-        br(),
-        tabsetPanel(
-            id = "inTabset",
-            tabPanel(title = h4("1. Fill in questionnaire"),
-                     value = 'p1',
-                     br(),
-                     p("On a scale from 1-9, where 1 is 'strongly disagree' and 9 is 'strongly agree', please answer the following:"),
-                     br(),
-                     sliderInput(inputId = 'q1',
-                                 label = h5(qs[1],style = "color:#044278"),
-                                 min = 1,
-                                 max = 9,
-                                 value = 5,
-                                 width = '100%'
-                     ),
-                     sliderInput(inputId = 'q2',
-                                 label = h5(qs[2],style = "color:#044278"),
-                                 min = 1,
-                                 max = 9,
-                                 value = 5,
-                                 width = '100%'
-                     ),
-                     sliderInput(inputId = 'q3',
-                                 label = h5(qs[3],style = "color:#044278"),
-                                 min = 1,
-                                 max = 9,
-                                 value = 5,
-                                 width = '100%'
-                     ),
-                     br(),
-                     h5(radioButtons(inputId = "carbon_price",
-                                     label = h4("I'd like to see results for:",style = "color:#044278"),
-                                     choices = c("Low carbon pricing" = 10,
-                                                 "Medium carbon pricing" = 30,
-                                                 "High carbon pricing" = 50),
-                                     inline = TRUE,
-                                     width = "100%")
-                     ),
-                     br(),
-                     actionButton('jumpToResults', 'See results')
-            ),
-            tabPanel(title = h4("2. View results"),
-                     value = 'p2',
-                     br(),
-                     h4(htmlOutput("carbonpricetxt")),
-                     br(),
-                     h4(htmlOutput("ind1")),
-                     plotOutput("p1"),
-                     br(),
-                     h4(htmlOutput("ind2")),
-                     plotOutput("p2"),
-                     br(),
-                     h4(htmlOutput("ind3")),
-                     plotOutput("p3"),
-                     br()
-            ),
-            tabPanel(title = h4("3. Share"),
-                     value = 'p3',
-                     h5('Your indicators'),
-                     br(),
-                     tableOutput('resultsTab'),
-                     downloadButton('downloadData', 'Download data')
-                     ),
-            tabPanel(title = h4("4. About"),
-                     value = 'p4',
-                     h5("About this app"),
-                     HTML("<p>This app is a prototype and estimates of carbon revenue should be considered crude estimates at best. Source code and information about data sources is available on <a  href = 'https://github.com/OpenDataServices/carpentr'>Github</a>. where you can also report issues or problems.</p>"),
-                     HTML("<p>Developed by <a href = 'https://opendataservices.coop/'>Open Data Services</a>, as part of the <a href = 'https://eiti.org/events/datathon-innovative-solutions-data-driven-energy-transition'>EITI Datathon 2022</a>.</p>")
-                     )
-        )
-    )
+                #theme = "bootstrap.css",
+                titlePanel(h1("Carbon pricing and the energy transition",style = "color:#cd2973")),
+                mainPanel(
+                  br(),
+                  tabsetPanel(
+                    id = "inTabset",
+                    tabPanel(title = h4("1. Fill in questionnaire"),
+                             value = 'p1',
+                             br(),
+                             p("On a scale from 1-9, where 1 is 'strongly disagree' and 9 is 'strongly agree', please answer the following:"),
+                             br(),
+                             sliderInput(inputId = 'q1',
+                                         label = h5(qs[1],style = "color:#044278"),
+                                         min = 1,
+                                         max = 9,
+                                         value = 5,
+                                         width = '100%'
+                             ),
+                             sliderInput(inputId = 'q2',
+                                         label = h5(qs[2],style = "color:#044278"),
+                                         min = 1,
+                                         max = 9,
+                                         value = 5,
+                                         width = '100%'
+                             ),
+                             sliderInput(inputId = 'q3',
+                                         label = h5(qs[3],style = "color:#044278"),
+                                         min = 1,
+                                         max = 9,
+                                         value = 5,
+                                         width = '100%'
+                             ),
+                             br(),
+                             h5(radioButtons(inputId = "carbon_price",
+                                             label = h4("I'd like to see results for:",style = "color:#044278"),
+                                             choices = c("Low carbon pricing" = 10,
+                                                         "Medium carbon pricing" = 30,
+                                                         "High carbon pricing" = 50),
+                                             inline = TRUE,
+                                             width = "100%")
+                             ),
+                             br(),
+                             actionButton('jumpToResults', 'See results')
+                    ),
+                    tabPanel(title = h4("2. View results"),
+                             value = 'p2',
+                             br(),
+                             h4(htmlOutput("carbonpricetxt")),
+                             br(),
+                             plotOutput("plots",height = "200px"),
+                             br()
+                    ),
+                    tabPanel(title = h4("3. Share"),
+                             value = 'p3',
+                             h5('Your indicators'),
+                             br(),
+                             tableOutput('resultsTab'),
+                             downloadButton('downloadData', 'Download data')
+                    ),
+                    tabPanel(title = h4("4. About"),
+                             value = 'p4',
+                             h5("About this app"),
+                             HTML("<p>This app is a prototype and estimates of carbon revenue should be considered crude estimates at best. Source code and information about data sources is available on <a  href = 'https://github.com/OpenDataServices/carpentr'>Github</a>, where you can also report issues.</p>"),
+                             HTML("<p>Developed by <a href = 'https://opendataservices.coop/'>Open Data Services</a>, as part of the <a href = 'https://eiti.org/events/datathon-innovative-solutions-data-driven-energy-transition'>EITI Datathon 2022</a>.</p>")
+                    )
+                  )
+                )
 )
 
 server <- function(input, output, session) {
 
-    carbonrev <- reactive({round(eiti_oilgas_carbon_revenue("SN",
-                                               2017,
-                                               as.numeric(input$carbon_price)/1000000))})
+  carbonrev <- reactive({round(eiti_oilgas_carbon_revenue("SN",
+                                                          2017,
+                                                          as.numeric(input$carbon_price)/1000000))})
 
-    oilgasrev <- reactive({
-      read_eiti('SN',2017,'revenue') %>%
-        mutate(value = round(as.numeric(value)/1000000)) %>%
-        pull(value) %>%
-        sum()})
+  oilgasrev <- reactive({
+    read_eiti('SN',2017,'revenue') %>%
+      mutate(value = round(as.numeric(value)/1000000)) %>%
+      pull(value) %>%
+      sum()})
 
-    output$carbonpricetxt <- renderText({
-        paste0("<span style = 'color:#044278'>There could have been </span><span style = 'color:#cd2973'>$",
-               carbonrev(),
-               " million USD</span><span style = 'color:#044278'> generated from carbon pricing in Senegal in 2017. This is </span><span style = 'color:#cd2973'>",
-               ifelse(carbonrev()<oilgasrev(),
-                      paste0(round(carbonrev()/oilgasrev()*100), "%</span><span style = 'color:#044278'> of"),
-                      paste0('$',carbonrev() - oilgasrev()," million more</span><span style = 'color:#044278'> than")),
-               " the entire government revenue generated from the extractives industries. This carbon pricing revenue could have been used for...</span>")
-    })
+  output$carbonpricetxt <- renderText({
+    paste0("<span style = 'color:#044278'>There could have been </span><span style = 'color:#cd2973'>$",
+           carbonrev(),
+           " million USD</span><span style = 'color:#044278'> generated from carbon pricing in Senegal in 2017. This is </span><span style = 'color:#cd2973'>",
+           ifelse(carbonrev()<oilgasrev(),
+                  paste0(round(carbonrev()/oilgasrev()*100), "%</span><span style = 'color:#044278'> of"),
+                  paste0('$',carbonrev() - oilgasrev()," million more</span><span style = 'color:#044278'> than")),
+           " the entire government revenue generated from the extractives industries. This carbon pricing revenue could have been used for...</span>")
+  })
 
-    dist <- reactive({
-        inputs <- c(input$q1,input$q2,input$q3)
-        apply(indicator_survey_matrix,1,euclidean,inputs)
-    })
+  dist <- reactive({
+    inputs <- c(input$q1,input$q2,input$q3)
+    apply(indicator_survey_matrix,1,euclidean,inputs)
+  })
 
-    userdata <- reactive({
-        tibble(name = inds[order(dist())][1:3]) %>%
-            left_join(indicator_raw) %>%
-            mutate(Rank = c(1:3))
-    })
-
-
-    output$ind1 <- renderText({
-        userdata()$description[1]
-    })
-
-    output$p1 <- renderPlot({
-        toplot <- indicator_values[indicator_values$name == userdata()$name[1],]
-        plot_indicator(toplot,carbonrev(),2017)
-    })
-
-    output$ind2 <- renderText({
-        userdata()$description[2]
-    })
-    output$p2 <- renderPlot({
-        toplot <- indicator_values[indicator_values$name == userdata()$name[2],]
-        plot_indicator(toplot,carbonrev(),2017)
-    })
+  userdata <- reactive({
+    tibble(name = inds[order(dist())][1:3]) %>%
+      left_join(indicator_raw) %>%
+      mutate(Rank = c(1:3))
+  })
 
 
-    output$ind3 <- renderText({
-        userdata()$description[3]
-      })
-    output$p3 <- renderPlot({
-        toplot <- indicator_values[indicator_values$name == userdata()$name[3],]
-        plot_indicator(toplot,carbonrev(),2017)
-    })
+  output$plots <- renderPlot({
 
-    output$resultsTab <- renderTable({
-        userdata() %>%
-            select(Indicator = description,
-                   Rank)
-        })
+    toplot <- indicator_values[indicator_values$name == userdata()$name[1],] %>%
+      bind_rows(indicator_values[indicator_values$name == userdata()$name[2],]) %>%
+      bind_rows(indicator_values[indicator_values$name == userdata()$name[3],]) %>%
+      mutate(lab = rep(userdata()$description,
+                       c(nrow(indicator_values[indicator_values$name == userdata()$name[1],]),
+                         nrow(indicator_values[indicator_values$name == userdata()$name[2],]),
+                         nrow(indicator_values[indicator_values$name == userdata()$name[3],]))
+      )
+      )
 
-    observeEvent(input$jumpToResults, {
-        updateTabsetPanel(session,
-                          "inTabset",
-                          selected = "p2")
-    })
+    plot_indicator(toplot,carbonrev(),2017)+
+      facet_wrap(~lab,scales = 'free')
 
-    output$downloadData <- downloadHandler(
-      filename = function() {
-        paste('data-', Sys.Date(), '.csv', sep='')
-      },
-      content = function(con) {
-        write.csv(        userdata() %>%
-                              select(Indicator = description,
-                                     Rank), con)
-      }
-    )
+    # p1 <- plot_indicator(indicator_values[indicator_values$name == userdata()$name[1],],
+    #                      carbonrev(),2017)
+    # p2 <- plot_indicator(indicator_values[indicator_values$name == userdata()$name[2],],
+    #                      carbonrev(),2017)
+    # p3 <- plot_indicator(indicator_values[indicator_values$name == userdata()$name[3],],
+    #                      carbonrev(),2017)
+    #
+    # plot_grid(p1,p2,p3,nrow = 1)
+
+  })
+
+  output$ind1 <- renderText({
+    userdata()$description[1]
+  })
+
+  output$p1 <- renderPlot({
+    toplot <- indicator_values[indicator_values$name == userdata()$name[1],]
+    plot_indicator(toplot,carbonrev(),2017)
+  })
+
+  output$ind2 <- renderText({
+    userdata()$description[2]
+  })
+  output$p2 <- renderPlot({
+    toplot <- indicator_values[indicator_values$name == userdata()$name[2],]
+    plot_indicator(toplot,carbonrev(),2017)
+  })
+
+
+  output$ind3 <- renderText({
+    userdata()$description[3]
+  })
+  output$p3 <- renderPlot({
+    toplot <- indicator_values[indicator_values$name == userdata()$name[3],]
+    plot_indicator(toplot,carbonrev(),2017)
+  })
+
+  output$resultsTab <- renderTable({
+    userdata() %>%
+      select(Indicator = description,
+             Rank)
+  })
+
+  observeEvent(input$jumpToResults, {
+    updateTabsetPanel(session,
+                      "inTabset",
+                      selected = "p2")
+  })
+
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste('data-', Sys.Date(), '.csv', sep='')
+    },
+    content = function(con) {
+      write.csv(        userdata() %>%
+                          select(Indicator = description,
+                                 Rank), con)
+    }
+  )
 }
 
 shinyApp(ui, server)
