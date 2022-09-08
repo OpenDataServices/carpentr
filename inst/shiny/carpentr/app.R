812e5cbe82cf0d8faebcc84523b5798767fe42af
Lewis Spurgin <lewisspurgin@gmail.com>
2022-09-08 14:38:18 +0100

    restructure

--- inst/shiny/carpentr/app.R
+++ inst/shiny/carpentr/app.R
@@ -14,77 +14,82 @@ indicator_raw <- read_csv('data-raw/indicators.csv')
 qs <- colnames(indicator_survey_matrix)
 inds <- rownames(indicator_survey_matrix)
 
-ui <- fluidPage(title = "Carbon pricing and the energy transition",
-                tags$style(type = "text/css", "a{color: #044278;}"),
-                setSliderColor(rep('#044278',3),c(1,2,3)),
-                #theme = "bootstrap.css",
-                titlePanel(h1("Carbon pricing and the energy transition",style = "color:#009966")),
-                mainPanel(
-                  br(),
-                  tabsetPanel(
-                    id = "inTabset",
-                    tabPanel(title = h4("1. Fill in questionnaire"),
-                             value = 'p1',
-                             br(),
-                             p("On a scale from 1-9, where 1 is 'strongly disagree' and 9 is 'strongly agree', please answer the following:"),
-                             br(),
-                             sliderInput(inputId = 'q1',
-                                         label = h5(qs[1],style = "color:#044278"),
-                                         min = 1,
-                                         max = 9,
-                                         value = 5,
-                                         width = '100%'
-                             ),
-                             sliderInput(inputId = 'q2',
-                                         label = h5(qs[2],style = "color:#044278"),
-                                         min = 1,
-                                         max = 9,
-                                         value = 5,
-                                         width = '100%'
-                             ),
-                             sliderInput(inputId = 'q3',
-                                         label = h5(qs[3],style = "color:#044278"),
-                                         min = 1,
-                                         max = 9,
-                                         value = 5,
-                                         width = '100%'
-                             ),
-                             br(),
-                             h5(radioButtons(inputId = "carbon_price",
-                                             label = h4("I'd like to see results for:",style = "color:#044278"),
-                                             choices = c("Low carbon pricing" = 10,
-                                                         "Medium carbon pricing" = 30,
-                                                         "High carbon pricing" = 50),
-                                             inline = TRUE,
-                                             width = "100%")
-                             ),
-                             br(),
-                             actionButton('jumpToResults', 'See results')
-                    ),
-                    tabPanel(title = h4("2. View results"),
-                             value = 'p2',
-                             br(),
-                             h4(htmlOutput("carbonpricetxt")),
-                             br(),
-                             plotOutput("plots",width = "500px",height = "500px"),
-                             br()
-                    ),
-                    tabPanel(title = h4("3. Share"),
-                             value = 'p3',
-                             h5('Your indicators'),
-                             br(),
-                             tableOutput('resultsTab'),
-                             downloadButton('downloadData', 'Download data')
-                    ),
-                    tabPanel(title = h4("4. About"),
-                             value = 'p4',
-                             h5("About this app"),
-                             HTML("<p>This app is a prototype and estimates of carbon revenue should be considered crude estimates at best. Source code and information about data sources is available on <a  href = 'https://github.com/OpenDataServices/carpentr'>Github</a>, where you can also report issues.</p>"),
-                             HTML("<p>Developed by <a href = 'https://opendataservices.coop/'>Open Data Services</a>, as part of the <a href = 'https://eiti.org/events/datathon-innovative-solutions-data-driven-energy-transition'>EITI Datathon 2022</a>.</p>")
-                    )
-                  )
-                )
-)
+ui <- navbarPage("Carbon pricing and the energy transition",collapsible = TRUE,
+                 id = "inTabset",
+                 tags$style(HTML("
+      .navbar-default .navbar-brand { display: none;}
+  ")),                 selected = "1. Fill in questionnaire",
+                 tabPanel("1. Fill in questionnaire",
+                          fluidPage(tags$style(type = "text/css", "a{color: #044278;}"),
+                                    setSliderColor(rep('#044278',3),c(1,2,3)),
+                                    #theme = "bootstrap.css",
+                                    title = h4("1. Fill in questionnaire"),
+                                    value = 'p1',
+                                    h1('Carbon pricing and the energy transition',style = 'color: #009966'),
+                                    br(),
+                                    h4(HTML("<span style = 'color:#044278'>Putting a price on carbon helps to bring down emissions and raises money for investment. But <span style = 'color:#009966'>how much money</span> , and <span style = 'color:#009966'>what could it be spent on?</span> First, tell us what you think about carbon pricing...</span>")),
+                                    br(),
+                                    sliderInput(inputId = 'q1',
+                                                label = h5(qs[1]),
+                                                min = 1,
+                                                max = 9,
+                                                value = 5,
+                                                width = '100%'
+                                    ),
+                                    sliderInput(inputId = 'q2',
+                                                label = h5(qs[2]),
+                                                min = 1,
+                                                max = 9,
+                                                value = 5,
+                                                width = '100%'
+                                    ),
+                                    sliderInput(inputId = 'q3',
+                                                label = h5(qs[3]),
+                                                min = 1,
+                                                max = 9,
+                                                value = 5,
+                                                width = '100%'
+                                    ),
+                                    br(),
+                                    h5(radioButtons(inputId = "carbon_price",
+                                                    label = h4("I'd like to see results for:",style = "color:#044278"),
+                                                    choices = c("Low carbon pricing" = 10,
+                                                                "Medium carbon pricing" = 30,
+                                                                "High carbon pricing" = 50),
+                                                    inline = TRUE,
+                                                    width = "100%"),
+                                    br(),
+                                    actionButton('viewResults', 'Go to results')
+                                    ))),
+                 tabPanel("2. View results",
+                          value = 'p2',
+                          fluidPage(align = 'center',
+                                    h1(align = 'left','Carbon pricing and the energy transition',style = 'color: #009966'),
+                                    br(),
+                                    h4(htmlOutput("carbonpricetxt"),align = 'left'),
+                                    br(),
+                                    plotOutput("plots",width = "500px",height = "500px"),
+                                    br()
+                          )),
+                 tabPanel("3. Share",
+                          fluidPage(
+                            value = 'p3',
+                            h1(align = 'left','Carbon pricing and the energy transition',style = 'color: #009966'),
+                            br(),
+                            h4('Your indicators',style = 'color:#044278'),
+                            br(),
+                            tableOutput('resultsTab'),
+                            downloadButton('downloadData', 'Download data')
+                          )),
+                 tabPanel("4. About",
+                          fluidPage(
+                            value = 'p4',
+                            h1(align = 'left','Carbon pricing and the energy transition',style = 'color: #009966'),
+                            h4('About this app',style = 'color:#044278'),
+                            HTML("<p>This app is a prototype and estimates of carbon revenue should be considered crude estimates at best. Source code and information about data sources is available on <a  href = 'https://github.com/OpenDataServices/carpentr'>Github</a>, where you can also report issues.</p>"),
+                            HTML("<p>Developed by <a href = 'https://opendataservices.coop/'>Open Data Services</a>, as part of the <a href = 'https://eiti.org/events/datathon-innovative-solutions-data-driven-energy-transition'>EITI Datathon 2022</a>.</p>")
+                          )))
+
 
 server <- function(input, output, session) {
 
@@ -134,9 +139,9 @@ server <- function(input, output, session) {
 
     plot_indicator(toplot,carbonrev(),2017)+
       facet_wrap(~lab,scales = 'free',ncol = 2) +
-    theme(legend.position = c(1, 0.22),
-          legend.justification = c(1, 0),
-          legend.text = element_text(size = 15))
+      theme(legend.position = c(1, 0.22),
+            legend.justification = c(1, 0),
+            legend.text = element_text(size = 15))
 
   })
 
@@ -146,7 +151,7 @@ server <- function(input, output, session) {
              Rank)
   })
 
-  observeEvent(input$jumpToResults, {
+  observeEvent(input$viewResults, {
     updateTabsetPanel(session,
                       "inTabset",
                       selected = "p2")
